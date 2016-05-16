86f7af4bea86c5cb06cc39bd35aea49b5d51d4c2
Rafael Francisco <rafaelfrancisco6@gmail.com>
2016-05-16 22:16:41 +0100

    lel

---
+++ TrabalhoDeSO2Cliente3/Utils.cpp
@@ -0,0 +1,29 @@
+#include "Utils.h"
+
+Jogo::Jogo()
+{
+}
+
+Jogo::~Jogo()
+{
+}
+
+void Jogo::setEstado(int e)
+{
+	estado = e;
+}
+
+int Jogo::getEstado()
+{
+	return estado;
+}
+
+Jogador::Jogador(string nome, int p)
+{
+	name = nome;
+	pid = p;
+}
+
+Jogador::~Jogador()
+{
+}
